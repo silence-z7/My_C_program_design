759156c9771e098facdb7f78ff43e2009ce8916e
Vergissmeinnicht <2547405171@qq.com>
2021-06-29 09:17:03 +0800

    对主函数模板进行修改

--- program/main.c
+++ program/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fun.h"
 int main()
 {
@@ -24,28 +25,41 @@ int main()
         switch (choice)
         {
         case 1:
+            system("pause");
             break;
         case 2:
+            system("pause");
             break;
         case 3:
+            system("pause");
             break;
         case 4:
+            system("pause");
             break;
         case 5:
+            system("pause");
             break;
         case 6:
+            system("pause");
             break;
         case 7:
+            system("pause");
             break;
         case 8:
+            system("pause");
             break;
         case 9:
+            system("pause");
             break;
         case 10:
+            system("pause");
             break;
         case 0:
+            system("pause");
             break;
-        default:printf("enter error! Please retype your choice.\n");
+        default:
+            printf("enter error! Please retype your choice.\n");
+            system("pause");
             break;
         }
     }
