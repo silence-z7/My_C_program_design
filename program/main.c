a8b8bff19fdaad2c011aec5303734b2b8dee365f
Vergissmeinnicht <2547405171@qq.com>
2021-07-01 02:35:05 +0800

    进行插入一条数据或删除一条时如果链表不存在，创建链表时如果链表已存在

--- program/main.c
+++ program/main.c
@@ -1,113 +1,174 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "fun.h"
 int main()
 {
     TeleBook *list = NULL;
     char choice_ch[CHOICE_SIZE];
     int choice;
-    char choice7;
+    char choice1, choice7;
     char outfile[20];
+    char infile[20];
     while (1)
     {
         Display_Main_Menu();
-        gets(choice_ch);
+        scanf("%s", choice_ch);
         if (Check(choice_ch, CHOICE_SIZE) == 0)
         {
-            printf("enter error! Please retype your choice.\n");
+            printf("\tenter error! Please retype your choice1.\n");
+            system("pause");
             continue;
         }
         choice = Str_to_num(choice_ch);
         if (choice == -1)
         {
-            printf("enter error! Please retype your choice.\n");
+            printf("\tenter error! Please retype your choice.\n");
+            system("pause");
             continue;
         }
         switch (choice)
         {
-        case 1:
-            printf ("\t Input Records \n");
-            Create();
+        case 1: //有序创建数据
+            //如果链表已存在，提示是否储存或抛弃该数据
+            if (list != NULL)
+            {
+                printf("\tList already exist.\n\tDo you want to store it first or abandon the data.\n");
+                printf("\t1.store\t2.abandon\n");
+                printf("\tPlease enter:");
+                while (1)
+                {
+                    scanf("%c", &choice1);
+                    while (choice1 == '\n')
+                        scanf("%c", &choice1);
+                    if (choice1 == '1')
+                    {
+                        printf("\tPlease input the name of the outfile:");
+                        scanf("%s", outfile);
+                        WritetoText(list, outfile);
+                    }
+                    else if (choice1 == '2')
+                        ;
+                    else
+                    {
+                        printf("\tEnter error. Please enter 1 or 2\n");
+                        printf("\tPlease enter:");
+                        continue;
+                    }
+                    Free_all(list);
+                    list = NULL;
+                    break;
+                }
+            }
+            printf("\tInput Records \n");
+            list = Create();
             system("pause");
             break;
-        case 2:
-            Display(list);
+        case 2: //分页显示数据
+            //如果链表中没有数据
+            if (list == NULL)
+                printf("\tThere is no data.\n");
+            else
+                Display(list);
             system("pause");
             break;
-        case 3:
-            printf("\t Insert a record:\n");
-		    list=Insert_a_record(list);
+        case 3: //插入一条数据
+            printf("\tInsert a record:\n");
+            list = Insert_a_record(list);
             system("pause");
             break;
-        case 4:
-            printf("\t Delete a record:\n");
-            list=Delete_a_record(list);
+        case 4: //删除一条数据
+            printf("\tDelete a record:\n");
+            list = Delete_a_record(list);
             system("pause");
             break;
-        case 5:
-            printf("\t Sort:\n");
-            Sort_by_num(list);
+        case 5: //排序
+            printf("\tSort:\n");
+            list = Sort_by_num(list);
             system("pause");
             break;
-        case 6:
-            printf("\t Query:\n");
-		    Query_a_record(list);
+        case 6: //查找数据
+            printf("\tQuery:\n");
+            Query_a_record(list);
             system("pause");
             break;
         //从文件中导入数据到链表
-        case 7:
+        case 7: //从文件导入数据
             //如果链表中已存在数据，应先向用户确认储存下该数据还是放弃数据
             if (list != NULL)
             {
-                printf("Data in the linked list is not stored. Do you want to stored them or abandon them?\n");
-                printf("1.store them.\n");
-                printf("2.abandon them.\n");
-                printf("enter 1 or 2 to give your choice:");
+                printf("\tData in the linked list is not stored. Do you want to stored them or abandon them?\n");
+                printf("\t1.store them.");
+                printf("\t2.abandon them.\n");
+                printf("\tenter 1 or 2 to give your choice:");
                 while (1)
                 {
                     scanf("%c", &choice7);
+                    while (choice7 == '\n')
+                        scanf("%c", &choice7);
                     if (choice7 == '1')
                     {
                         //此处补充储存数据代码
+                        printf("\tPlease input the name of the outfile:");
+                        scanf("%s", outfile);
+                        WritetoText(list, outfile);
                         break;
                     }
                     if (choice7 == '2')
                         ;
-                    //先释放空间
-                    //读取文件数据并覆盖链表数据代码
                     else
-                        printf("Enter error! Please give your choice in 1 or 2.\n");
+                    {
+                        printf("\tEnter error! Please give your choice in 1 or 2.\n");
+                        continue;
+                    }
+                    //先释放空间
+                    Free_all(list);
+                    list = NULL;
+                    //读取文件数据
+                    printf("\tPlease input the name of the infile:");
+                    scanf("%s", infile);
+                    list = AddfromText(list, infile);
+                    break;
                 }
+            }
+            //如果链表中没有数据，则导入数据
+            else
+            {
+                printf("\tPlease input the name of the infile:");
+                scanf("%s", infile);
+                list = AddfromText(list, infile);
             }
             system("pause");
             break;
-        case 8:
-            printf("\t Write to a text file \n");
-            WritetoText(list,outfile);
+        case 8: //向文件写入数据
+            printf("\tWrite to a text file \n");
+            printf("\tPlease input the name of the outfile:");
+            scanf("%s", outfile);
+            WritetoText(list, outfile);
             system("pause");
             break;
-        case 9:
-            printf("\t Reverse List\n");
-		    list=Reverse(list);
+        case 9: //链表反序
+            printf("\tReverse List\n");
+            list = Reverse(list);
             system("pause");
             break;
-        case 10:
-            printf("\t Delete the same record:\n");
-		    list=DeleteSame(list);
+        case 10: //删除相同数据
+            printf("\tDelete the same record:\n");
+            list = DeleteSame(list);
             system("pause");
             break;
-        case 11:
-            printf("Alter existing records");
-            list=Alter_list(list);
+        case 11: //修改一条数据
+            printf("\tAlter existing records");
+            list = Alter_list(list);
             system("pause");
             break;
-        case 0:
-            printf("\t Quit\n");
-		    Quit(list);
+        case 0: //退出
+            printf("\tQuit\n");
+            Quit(list);
             system("pause");
             break;
         default:
-            printf("enter error! Please retype your choice.\n");
+            printf("\tEnter error! Please retype your choice.\n");
             system("pause");
             break;
         }
